65b5417223e9ffec89110c79381f0d9fc1a416e8
StevenJWright_cp <SND\StevenJWright_cp>
2012-05-10 18:01:57 +0000

    Updated the solution / project to visual studio 2010.  Also updated task to exposed custom porperties to custom the XML output.

---
+++ Xmlify/Tjoc.SqlServer.Dts.Pipeline.Xmlify/HelperClasses/ColumnInfo.cs
@@ -0,0 +1,47 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ColumnInfo.cs" company="Codeplex">
+//   
+// </copyright>
+// <summary>
+//   The column info.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Tjoc.SqlServer.Dts.Pipeline.Xmlify.HelperClasses
+{
+    #region using directive
+
+    using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
+
+    #endregion
+
+    /// <summary>
+    /// The column info.
+    /// </summary>
+    internal class ColumnInfo
+    {
+        #region Public Properties
+
+        /// <summary>
+        ///   The buffer column index.
+        /// </summary>
+        public int BufferColumnIndex { get; set; }
+
+        /// <summary>
+        ///   The column disposition.
+        /// </summary>
+        public DTSRowDisposition ColumnDisposition { get; set; }
+
+        /// <summary>
+        ///   The lineage id.
+        /// </summary>
+        public int LineageId { get; set; }
+
+        /// <summary>
+        ///   The name.
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
